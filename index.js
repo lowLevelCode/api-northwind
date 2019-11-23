345b348dd4c3e4ade1d6592cd2bf212853bd4e84
jesus raul alarcon ontiveros <thelowlevelcode@gmail.com>
2019-11-23 00:55:42 -0600

    agregamos la conexion de mogoose

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 'use strict'
 
-const app         = require('./app');
+const app           = require('./app');
+const mongoose      = require('mongoose');
 
-app.listen(3900,() => {
-    console.log("servidor corriendo");   
-});
+mongoose.connect('mongodb://localhost/northwind', { useNewUrlParser: true, useUnifiedTopology: true }).then( 
+    () => { 
+        console.log("Conexion abierta"); 
+        app.listen(3900,() => {
+            console.log("servidor corriendo");   
+        });
+    },     
+    err => { console.err("error al abrir conexion"); }     
+);
