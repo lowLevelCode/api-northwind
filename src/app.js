932d6be2b19eae93f2ffd596a1954de20ca8666a
jesus raul alarcon ontiveros <thelowlevelcode@gmail.com>
2019-11-23 02:47:07 -0600

    cambio de carpeta a src

---
+++ src/app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var fs          = require('fs');
+
+var app         = express();
+
+// Middlewares
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+// Enrutamiento de los controllers
+fs.readdirSync('src/controllers').forEach( (file) => 
+{  
+    if(file.substr(-3) == '.js')
+    {    
+        const route = require('./controllers/' + file);
+        route.controller(app)  
+    }
+});
+
+module.exports = app;
